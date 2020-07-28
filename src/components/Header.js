53fda0eee72fa60fb59648fffa299dd8a68f33bb
rsoto003 <rsoto003@ucr.edu>
2020-07-28 13:11:23 -0700

    adding new repository, added necessary packages to application, created Header component

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+ import React from 'react';
+
+ const Header = props => {
+     return (
+        <div className="pb-2 mt-4 mb-2 border-bottom">
+            { /** only visible when not on mobile */}
+            <h1 className="d-none d-md-block">Student Grade Table
+                <small className="float-right"> Grade Average: <span className="text-warning">0</span></small>
+            </h1>
+            { /** only visible when user is on mobile */}
+            <h4 className="d-sm-none">Student Grade Table
+                <small className="float-right"> Grade Average: <span className="text-danger">0</span></small>
+            </h4>
+        </div>
+     );
+ }
+
+ export default Header;
