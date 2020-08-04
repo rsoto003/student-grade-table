bf20ee0754874cf0790ec41ba3bf0d303a278e5c
rsoto003 <rsoto003@ucr.edu>
2020-08-04 13:31:28 -0700

    calculateGrades method now functional, need to check for bugs when editing/deleting student

--- src/App.js
+++ src/App.js
@@ -9,30 +9,43 @@ class App extends Component {
     name: '',
     course: '',
     grade: '',
+    totalGrade: 0,
     id: '',
-    students: []
+    idEditing: false,
+    students: [],
+    overallGrade: 0,
   }
+
   handleChange = event => {
     const { name, value } = event.target;
     this.setState({ 
-      [name] : value
+      [name] : value,
     });
   }
 
   handleSubmit = event => {
     event.preventDefault();
     let { name, course, grade, id, students } = this.state;
+    let total = 0;
+    total+= parseInt(grade);
     id = students.length + 1;
     const studentData = {
       name, course, grade, id
     }
-    this.setState({ 
-      students: [...this.state.students, studentData],
+    
+    this.setState(prevState => ({ 
+      students: [...students, studentData],
       name: '',
       course: '',
       grade: '', 
-      id: ''
-    });
+      id: '',
+      totalGrade: prevState.totalGrade + total,
+    }));
+
+    setTimeout( 
+      () => this.calculateGrades(), 
+      100
+    );
   }
 
   cancelClick = () => {
@@ -51,16 +64,62 @@ class App extends Component {
         students: updatedStudentsList
       }
     });
+    setTimeout( 
+      () => this.calculateGrades(), 
+      100
+    );
   }
 
+  editChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  }
 
+  editStudent = id => {
+    console.log('student data to be edited: ', id);
+  }
+
+  editSubmit = event => {
+    event.preventDefault();
+
+  }
+
+  calculateGrades = () => {
+    const { overallGrade, students }= this.state;
+    console.log(students.length);
+    let totalGrade = 0;
+    let gradeAverage = 0;
+    for(let i = 0; i < students.length; i++ ){
+      totalGrade+= parseInt(students[i].grade);
+    }
+    gradeAverage = parseFloat(totalGrade / students.length).toFixed(2);
+    this.setState({ 
+      overallGrade: gradeAverage
+    });
+    return overallGrade;
+  }
 
   render(){
     return (
       <div className="container text-white lead">
-        <Header/>
-        <AddForm handleChange={this.handleChange} studentData={this.state} handleSubmit={this.handleSubmit} cancelClick={this.cancelClick}/>
-        <StudentContainer students={this.state.students} deleteStudent={this.deleteStudent}/>
+        <Header 
+          grade={this.state.overallGrade}
+          totalGrade={this.state.totalGrade}
+          students={this.state.students}
+        />
+        <AddForm 
+          handleChange={this.handleChange} 
+          studentData={this.state} 
+          handleSubmit={this.handleSubmit} 
+          cancelClick={this.cancelClick}
+
+        />
+        <StudentContainer 
+          students={this.state.students} 
+          deleteStudent={this.deleteStudent}
+        />
       </div>
     );
   }
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,11 +1,11 @@
  import React from 'react';
 
- const Header = props => {
+ const Header = ({grade}) => {
      return (
         <div className="pb-2 mt-4 mb-5 border-bottom">
             { /** only visible when not on mobile */}
             <h2 className="d-none d-md-block">Student Grade Table
-                <small className="float-right"> Grade Average: <span className="text-warning">0</span></small>
+     <small className="float-right"> Grade Average: <span className="text-warning">{grade}</span></small>
             </h2>
             { /** only visible when user is on mobile */}
             <h4 className="d-sm-none">Student Grade Table
